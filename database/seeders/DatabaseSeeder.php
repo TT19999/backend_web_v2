e046bd95051455ca186fa4f79d14f54370395dc2
unknown <tunghsut99@gmail.com>
2020-11-08 23:47:20 +0700

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,31 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('permissions')->insert([
-            ['name' => 'view_info'],
-            ['name' => 'update_info'],
-            ['name' => 'delete_info'],
-            ['name' => 'restore_info'],
-            ['name' => 'view_trip'],
-            ['name' => 'update_trip'],
-            ['name' => 'delete_trip'],
-            ['name' => 'restore_trip'],
-            ['name' => 'create_trip'],
-        ]);
-        
-        DB::table('roles')->insert([
-            ['name' => 'admin'],
-            ['name' => 'user'],
-            ['name' => 'contributor'],
-        ]);
-        DB::table('role_user')->insert([
-            'role_id' => 2,
-            'user_id' => 1,
-        ]);
-        DB::table('role_user')->insert([
-            'role_id' => 3,
-            'user_id' => 2,
-        ]);
         DB::table('permission_role')->insert([
             ['permission_id' => 1, 'role_id' => 1],
             ['permission_id' => 2, 'role_id' => 1],
