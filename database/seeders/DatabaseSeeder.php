8b174c7a63ec172f62f595e4568a6911b20a7475
unknown <tunghsut99@gmail.com>
2020-11-08 23:45:36 +0700

    .

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -39,21 +39,21 @@ class DatabaseSeeder extends Seeder
             'user_id' => 2,
         ]);
         DB::table('permission_role')->insert([
-            ['permisison_id' => 1, 'role_id' => 1],
-            ['permisison_id' => 2, 'role_id' => 1],
-            ['permisison_id' => 3, 'role_id' => 1],
-            ['permisison_id' => 4, 'role_id' => 1],
+            ['permission_id' => 1, 'role_id' => 1],
+            ['permission_id' => 2, 'role_id' => 1],
+            ['permission_id' => 3, 'role_id' => 1],
+            ['permission_id' => 4, 'role_id' => 1],
         ]);
         DB::table('permission_role')->insert([
-            ['permisison_id' => 1, 'role_id' => 2],
-            ['permisison_id' => 2, 'role_id' => 2],
-            ['permisison_id' => 4, 'role_id' => 2],
+            ['permission_id' => 1, 'role_id' => 2],
+            ['permission_id' => 2, 'role_id' => 2],
+            ['permission_id' => 4, 'role_id' => 2],
         ]);
         DB::table('permission_role')->insert([
-            ['permisison_id' => 9, 'role_id' => 3],
-            ['permisison_id' => 1, 'role_id' => 3],
-            ['permisison_id' => 2, 'role_id' => 3],
-            ['permisison_id' => 4, 'role_id' => 3],
+            ['permission_id' => 9, 'role_id' => 3],
+            ['permission_id' => 1, 'role_id' => 3],
+            ['permission_id' => 2, 'role_id' => 3],
+            ['permission_id' => 4, 'role_id' => 3],
         ]);
     }
 }
